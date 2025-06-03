d5f9e04929ddcd4c88de12d2c8ec8d58aa8d020a
cremacious <chrismackall3@gmail.com>
2025-06-03 14:31:10 -0400

    Working Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from '../config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from '../middleware/rateLimiter.js';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -12,6 +13,11 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(rateLimiter);
+app.use(
+  cors({
+    origin: 'http://localhost:5173', // Remove trailing slash
+  })
+);
 
 app.use('/api/notes', notesRoutes);
 
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,10 +1,54 @@
+import { Navbar } from '../components/Navbar';
+import { useState, useEffect } from 'react';
+import RateLimitedUI from '../components/RateLimitedUI';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+
 const Homepage = () => {
+  const [isRateLimited, setRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/api/notes');
+        console.log(res.data);
+        setNotes(res.data);
+        setRateLimited(false);
+      } catch (error) {
+        console.log(error.response);
+        if (error.response?.status === '429') {
+          setRateLimited(true);
+        } else {
+          toast.error('Error retrieving notes.');
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
   return (
-    <>
-      Homepage
-      <button className="btn">Button</button>
-      <div className="bg-red-500">Homepage</div>
-    </>
+    <div className="min-h-screen">
+      <Navbar />
+      {isRateLimited && <RateLimitedUI />}
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && (
+          <div className="text-center text-primary py-10">Loading notes...</div>
+        )}
+
+        {/* {notes.length === 0 && !isRateLimited && <NotesNotFound />} */}
+        {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
